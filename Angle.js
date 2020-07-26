62d984b29a25257db29f3f8c10353cba2de4fbda
dikshadodanisainani <54570385+dikshadodanisainani@users.noreply.github.com>
2020-07-26 13:14:15 +0530

    Create Angle.js

---
+++ Angle.js
@@ -0,0 +1,22 @@
+var angle = 10;
+
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(220);
+  
+//push();
+ //translate(200,200);
+  //rotate(angle);
+ //rect(0,0,50,100);
+  //pop();
+  //rect(100,100,50,100);
+  //angle = angle + 10;
+  
+  //rect(50,50,50,100);
+  
+}
